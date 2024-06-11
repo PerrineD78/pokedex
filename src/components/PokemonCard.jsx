c79c8aeb68549f0f4e35cce9439b91dcd68e871e
Perrine Deserville <mondierep@gmail.com>
2024-06-11 15:34:29 +0200

    components.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
+const pokemonList = [
+    {
+      name: "Bulbizarre",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard () {
+const pokemon = pokemonList[0]
+
     return (
         <figure>
-            <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-            <figcaption>Bulbizarre</figcaption>
+           { pokemon.imgSrc ? <img src ={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p> }
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 } 
