f0f22f7a54c310d88a95758163f773d2d412e790
Perrine Deserville <mondierep@gmail.com>
2024-06-11 17:18:33 +0200

    react4

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+const pokemonList = [
+  {
+    name: "Bulbizarre",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <PokemonCard/>
+      <PokemonCard pokemon={pokemonList[0]} />
       </div>
     </>
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,5 @@
-const pokemonList = [
-    {
-      name: "Bulbizarre",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard () {
-const pokemon = pokemonList[0]
+function PokemonCard ({pokemon}) {
 
     return (
         <figure>
